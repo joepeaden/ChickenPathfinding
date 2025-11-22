045437e5f36a6ba7f498f1b6f6c57e820e59cdc3
Joseph <joe.peaden@gmail.com>
2025-11-22 13:45:52 -0500

    Added FPS logger and fixed build error with editor file

---
+++ Assets/Scripts/FPSLogger.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+public class FPSLogger : MonoBehaviour
+{
+    #if !UNITY_EDITOR
+    private List<float> fpsValues = new List<float>();
+    private float nextSampleTime;
+
+    private void Start()
+    {
+        nextSampleTime = Time.time + 1f;
+    }
+
+    private void Update()
+    {
+        if (Time.time >= nextSampleTime)
+        {
+            float fps = 1f / Time.deltaTime;
+            fpsValues.Add(fps);
+            nextSampleTime += 1f;
+
+            if (fpsValues.Count >= 10)
+            {
+                float average = fpsValues.Average();
+                WriteToFile(average);
+                enabled = false; // Stop logging after first 10 seconds
+            }
+        }
+    }
+
+    private void WriteToFile(float averageFps)
+    {
+        string exeFolder = Path.GetDirectoryName(Application.dataPath);
+        string filePath = Path.Combine(exeFolder, "fps_log.txt");
+        using (StreamWriter writer = new StreamWriter(filePath, true))
+        {
+            writer.WriteLine($"{System.DateTime.Now}: Average FPS over first 10 seconds: {averageFps:F2}");
+        }
+    }
+    #endif
+}
